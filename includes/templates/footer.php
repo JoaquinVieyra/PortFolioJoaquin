0b8fb6e4d63c7faab5468095acd310e4078223b8
joaquin <joaquinvieyra2001@gmail.com>
2023-05-17 04:42:11 -0300

    Cambios y finalizacion de la seccion de contacto

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,3 +1,7 @@
+
 <script src="/public/build/js/js.js"></script>
 </body>
+<footer>
+    <p>&copy; <?php echo date('Y') ?> Mi Sitio Web. Todos los derechos reservados.</p>
+</footer>
 </html>
--- includes/templates/menu-principal.php
+++ includes/templates/menu-principal.php
@@ -33,7 +33,7 @@
 <div class="contenedor seccion curriculum" id="paso-3">
     <h1 class="seccion-titulo">Mi <span>Curriculum</span></h1>
     <div class="curriculum">
-        <p>Descargar/Download: <a href="#">Espaniol</a> - <a href="#">Ingles</a></p>
+        <p>Descargar/Download: <a href="https://drive.google.com/file/d/1L6sS5c2f07T3HoHLxdOl1VC1VCUTYtGW/view?usp=sharing" target="_blank">Espaniol</a> - <a href="https://drive.google.com/file/d/1cmT4ROnvbgsrTnigZvGwKhyuX7UAeVjf/view?usp=sharing " target="_blank">English</a></p>
         <picture>
             <source srcset="/public/build/img/cv.webp" type="image/webp">
             <source srcset="/public/build/img/cv.jpg" type="image/jpeg">
@@ -68,13 +68,15 @@
             </svg>
         </div> <!-- Red-->
         <div class="red">
-            <a href="https://www.linkedin.com/in/joaquin-vieyra-4780991b7/" target="_blank">
-                <h3>GitHub</h3>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSf96yDd22QWy7o9NiF0hsR0WM2zrNQnrVd9gGRM1tXqZbAoSA/viewform?usp=sf_link" target="_blank">
+                <h3>Email</h3>
             </a>
-
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-github-filled" width="48" height="48" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-gmail" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M5.315 2.1c.791 -.113 1.9 .145 3.333 .966l.272 .161l.16 .1l.397 -.083a13.3 13.3 0 0 1 4.59 -.08l.456 .08l.396 .083l.161 -.1c1.385 -.84 2.487 -1.17 3.322 -1.148l.164 .008l.147 .017l.076 .014l.05 .011l.144 .047a1 1 0 0 1 .53 .514a5.2 5.2 0 0 1 .397 2.91l-.047 .267l-.046 .196l.123 .163c.574 .795 .93 1.728 1.03 2.707l.023 .295l.007 .272c0 3.855 -1.659 5.883 -4.644 6.68l-.245 .061l-.132 .029l.014 .161l.008 .157l.004 .365l-.002 .213l-.003 3.834a1 1 0 0 1 -.883 .993l-.117 .007h-6a1 1 0 0 1 -.993 -.883l-.007 -.117v-.734c-1.818 .26 -3.03 -.424 -4.11 -1.878l-.535 -.766c-.28 -.396 -.455 -.579 -.589 -.644l-.048 -.019a1 1 0 0 1 .564 -1.918c.642 .188 1.074 .568 1.57 1.239l.538 .769c.76 1.079 1.36 1.459 2.609 1.191l.001 -.678l-.018 -.168a5.03 5.03 0 0 1 -.021 -.824l.017 -.185l.019 -.12l-.108 -.024c-2.976 -.71 -4.703 -2.573 -4.875 -6.139l-.01 -.31l-.004 -.292a5.6 5.6 0 0 1 .908 -3.051l.152 -.222l.122 -.163l-.045 -.196a5.2 5.2 0 0 1 .145 -2.642l.1 -.282l.106 -.253a1 1 0 0 1 .529 -.514l.144 -.047l.154 -.03z" stroke-width="0" fill="currentColor" />
+                <path d="M16 20h3a1 1 0 0 0 1 -1v-14a1 1 0 0 0 -1 -1h-3v16z" />
+                <path d="M5 20h3v-16h-3a1 1 0 0 0 -1 1v14a1 1 0 0 0 1 1z" />
+                <path d="M16 4l-4 4l-4 -4" />
+                <path d="M4 6.5l8 7.5l8 -7.5" />
             </svg>
         </div> <!-- Red-->
 
