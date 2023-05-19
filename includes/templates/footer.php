1de110a4cede2d5a104986fa5577740565e4ff08
joaquin <joaquinvieyra2001@gmail.com>
2023-05-19 16:16:41 -0300

    arreglando errores en la vista de celular

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,7 +1,7 @@
 
 <script src="/public/build/js/js.js"></script>
 </body>
-<footer>
+<footer class="footer-separator">
     <p>&copy; <?php echo date('Y') ?> Mi Sitio Web. Todos los derechos reservados.</p>
 </footer>
 </html>
